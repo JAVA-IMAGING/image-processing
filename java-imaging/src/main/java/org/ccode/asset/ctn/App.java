14834cc3454a3f72986fd1d959bb26e823e15ce9
wwww-teddy <yddet.www@gmail.com>
2024-09-23 19:32:52 -0500

    Squashed commit of the following:
    commit 9497b09460667493f63fb8e54d12b9242359025c
    Author: wwww-teddy <yddet.www@gmail.com>
    Date:   Mon Sep 23 19:31:55 2024 -0500
    
        Didn't like the look
    
    commit 86ee1d5b1e8c1bbace658a83f305a2ea18e9e95a
    Author: wwww-teddy <yddet.www@gmail.com>
    Date:   Mon Sep 23 19:31:37 2024 -0500
    
        Creating wrappers for Maven

--- java-imaging/src/main/java/org/ccode/asset/ctn/App.java
+++ java-imaging/src/main/java/org/ccode/asset/ctn/App.java
@@ -4,10 +4,8 @@ package org.ccode.asset.ctn;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
