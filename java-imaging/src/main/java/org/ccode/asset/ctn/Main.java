b7c0f6380bab31ecc2153b7264ab0a3a7fc266cf
wwww-teddy <yddet.www@gmail.com>
2024-10-01 12:51:07 -0500

    Added notes about testing

--- java-imaging/src/main/java/org/ccode/asset/ctn/Main.java
+++ java-imaging/src/main/java/org/ccode/asset/ctn/Main.java
@@ -13,16 +13,22 @@ import java.io.FileNotFoundException;
  */
 public class Main {
     public static void main( String[] args ) throws FileNotFoundException {
-        String bias = "java-imaging/src/test/fits/20181002_16.fits";
-        String flat = "java-imaging/src/test/fits/20181002_16.fits";
-        Fits test1 = new Fits(flat);
+        /**
+         * Notes about the whole thing:
+         * - When processing the FITS images, the value of BITPIX determines the datatype.
+         * - If BITPIX is negative, data are floats. If it is positive, data are integers.
+         */
+        String img1 = "java-imaging/src/test/fits/20181002_16.fits";    // data is in integer
+        String img2 = "java-imaging/src/test/fits/20181002_17.fits";    // data is in integer
+        String img3 = "java-imaging/src/test/fits/20181002_28.fits";    // this should be in floats automatically
+        Fits test1 = new Fits(img3);
 
        //  Fits[] test2 = Fits.collectObjects(Constants.testFitsPath, Constants.HeaderObjs.BIAS_IMG);
 
         System.out.println(test1);
 
-        float[][] extractFloatTest = FitsDocument.extractFloatData(test1);
+        float[][] extractFloatTest = FitsDocument.extractFloatData(test1); // problem is SOME of the extracted FITS data is in short, not float
 
-        System.out.println(extractFloatTest);
+        System.out.println(Array2D.toString(extractFloatTest));
     }
 }
